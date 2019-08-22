f06f4e16110a678d310253c05b27f648339ce7fe
Alexey Golubev <Alexey.Golubev@onlyoffice.com>
2019-08-22 15:55:35 +0300

    Initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,138 @@
+pipeline {
+  agent none
+  parameters {
+    booleanParam (
+      defaultValue: true,
+      description: '',
+      name: 'clean'
+    )
+    booleanParam (
+      defaultValue: true,
+      description: '',
+      name: 'linux_64'
+    )
+    booleanParam (
+      defaultValue: true,
+      description: '',
+      name: 'win_64'
+    )
+    booleanParam (
+      defaultValue: true,
+      description: '',
+      name: 'win_32'
+    )
+    booleanParam (
+      defaultValue: true,
+      description: '',
+      name: 'win_64_xp'
+    )
+    booleanParam (
+      defaultValue: true,
+      description: '',
+      name: 'win_32_xp'
+    )
+  }
+  triggers {
+    cron('@midnight')
+  }
+  stages {
+    stage('Prepare') {
+      steps {
+        script {
+          def branchName = env.BRANCH_NAME
+          def productVersion = "5.3.99"
+          def pV = branchName =~ /^(release|hotfix)\\/v(.*)$/
+          if(pV.find()) {
+            productVersion = pV.group(2)
+          }
+          env.PRODUCT_VERSION = productVersion
+        }
+      }
+    }
+    stage('Build') {
+      parallel {
+        stage('Linux 64-bit build') {
+          agent { label 'linux_64' }
+          steps {
+            script {
+              def utils = load "utils.groovy"
+              if ( params.linux_64 ) {
+                utils.linuxBuild(env.BRANCH_NAME, "linux_64", params.clean)
+              }
+            }
+          }
+        }
+        stage('Windows 64-bit build') {
+          agent {
+            node {
+              label 'win_64'
+              customWorkspace "C:\\oo\\${env.BRANCH_NAME}\\win_64"
+            }
+          }
+          steps {
+            script {
+              def utils = load "utils.groovy"
+              if ( params.win_64 ) {
+                utils.windowsBuild(env.BRANCH_NAME, "win_64", params.clean)
+              }
+            }
+          }
+        }
+        stage('Windows 32-bit build') {
+          agent {
+            node {
+              label 'win_32'
+              customWorkspace "C:\\oo\\${env.BRANCH_NAME}\\win_32"
+            }
+          }
+          steps {
+            script {
+              def utils = load "utils.groovy"
+              if ( params.win_32 ) {
+                utils.windowsBuild(env.BRANCH_NAME, "win_32", params.clean)
+              }
+            }
+          }
+        }
+        stage('Windows XP 64-bit build') {
+          agent {
+            node {
+              label 'win_64_xp'
+              customWorkspace "C:\\oo\\${env.BRANCH_NAME}\\win_64_xp"
+            }
+          }
+          environment {
+            _WIN_XP = '1'
+          }
+          steps {
+            script {
+              def utils = load "utils.groovy"
+              if ( params.win_64_xp ) {
+                utils.windowsBuild(env.BRANCH_NAME, "win_64_xp", params.clean)
+              }
+            }
+          }
+        }
+        stage('Windows XP 32-bit build') {
+          agent {
+            node {
+              label 'win_32_xp'
+              customWorkspace "C:\\oo\\${env.BRANCH_NAME}\\win_32_xp"
+            }
+          }
+          environment {
+            _WIN_XP = '1'
+          }
+          steps {
+            script {
+              def utils = load "utils.groovy"
+              if ( params.win_32_xp ) {
+                utils.windowsBuild(env.BRANCH_NAME, "win_32_xp", params.clean)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
---
+++ utils.groovy
@@ -0,0 +1,157 @@
+def checkoutRepo(String repo, String branch = 'master') {
+    checkout([
+            $class: 'GitSCM',
+            branches: [[
+                    name: branch
+                ]
+            ],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [[
+                    $class: 'RelativeTargetDirectory',
+                    relativeTargetDir: repo
+                ]
+            ],
+            submoduleCfg: [],
+            userRemoteConfigs: [[
+                    url: 'git@github.com:ONLYOFFICE/' + repo + '.git'
+                ]
+            ]
+        ]
+    )
+}
+
+return this
+
+def getReposList()
+{
+    def repos = []
+    repos.add('build_tools')
+    repos.add('core')
+    repos.add('desktop-apps')
+    repos.add('desktop-sdk')
+    repos.add('dictionaries')
+    repos.add('document-builder-package')
+    repos.add('sdkjs')
+    repos.add('sdkjs-plugins')
+    repos.add('web-apps-pro')
+    repos.add('DocumentBuilder')
+    return repos
+}
+
+def checkoutRepos(String branch = 'master')
+{    
+    for (repo in getReposList()) {
+        checkoutRepo(repo, branch)
+    }
+
+    return this
+}
+
+def tagRepos(String tag)
+{
+    for (repo in getReposList()) {
+        sh "cd ${repo} && \
+            git tag -l | xargs git tag -d && \
+            git fetch --tags && \
+            git tag v${env.PRODUCT_VERSION}.${env.BUILD_NUMBER} && \
+	        git push origin --tags"
+    }
+
+    return this
+}
+def linuxBuild(String branch = 'master', String platform = 'native', Boolean clean = true)
+{
+    checkoutRepos(branch)
+    sh "cd build_tools && \
+        ./configure \
+            --module \"desktop builder\"\
+            --platform ${platform}\
+            --update false\
+            --branch ${branch}\
+            --clean ${clean.toString()}\
+            --deploy\
+            --qt-dir \$QT_PATH &&\
+        ./make"
+    sh "cd desktop-apps/win-linux/package/linux &&\
+         make clean &&\
+         make deploy"
+    sh "cd document-builder-package &&\
+         make clean &&\
+         make deploy"
+
+    publishHTML([
+            allowMissing: false,
+            alwaysLinkToLastBuild: false,
+            includes: 'index.html',
+            keepAll: true,
+            reportDir: 'desktop-apps/win-linux/package/linux',
+            reportFiles: 'index.html',
+            reportName: "DesktopEditors(${platform})",
+            reportTitles: ''
+        ]
+    )
+
+    publishHTML([
+            allowMissing: false,
+            alwaysLinkToLastBuild: false,
+            includes: 'index.html',
+            keepAll: true,
+            reportDir: 'document-builder-package',
+            reportFiles: 'index.html',
+            reportName: "DocumentBuilder(${platform})",
+            reportTitles: ''
+        ]
+    )
+    return this
+}
+
+def windowsBuild(String branch = 'master', String platform = 'native', Boolean clean = true)
+{
+    checkoutRepos(branch)
+
+    bat "cd build_tools &&\
+            call configure.bat\
+            --module \"desktop builder tests updmodule\"\
+            --platform ${platform}\
+            --update false\
+            --branch ${branch}\
+            --clean ${clean.toString()}\
+            --deploy\
+            --qt-dir \"C:\\Qt\\Qt5.9.8\\5.9.8\"\
+            --qt-dir-xp \"C:\\Qt\\Qt5.6.3\\5.6.3\" &&\
+            call make.bat"
+
+    bat "cd desktop-apps &&\
+            mingw32-make clean-package &&\
+            mingw32-make package win-linux/package/windows/index.html"
+
+    publishHTML([
+            allowMissing: false,
+            alwaysLinkToLastBuild: false,
+            includes: 'index.html',
+            keepAll: true,
+            reportDir: 'desktop-apps/win-linux/package/windows',
+            reportFiles: 'index.html',
+            reportName: "DesktopEditors(${platform})",
+            reportTitles: ''
+        ]
+    )
+
+    if ( !platform.endsWith('_xp') ) {
+        bat "cd document-builder-package &&\
+            mingw32-make clean &&\
+            mingw32-make deploy"
+
+        publishHTML([
+                allowMissing: true,
+                alwaysLinkToLastBuild: false,
+                includes: 'index.html',
+                keepAll: true,
+                reportDir: 'document-builder-package',
+                reportFiles: 'index.html',
+                reportName: "DocumentBuilder(${platform})",
+                reportTitles: ''
+            ]
+        )
+    }
+}
