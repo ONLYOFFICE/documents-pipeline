e966452eddeb285030773d96395c4a8ca5f26416
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2020-07-15 18:51:34 +0300

    Automate flow (#36)

--- utils.groovy
+++ utils.groovy
@@ -1,4 +1,4 @@
-def checkoutRepo(String repo, String branch = 'master', String dir, String company = 'ONLYOFFICE') {
+def checkoutRepo(String repo, String branch = 'master', String dir = repo, String company = 'ONLYOFFICE') {
     checkout([
             $class: 'GitSCM',
             branches: [[
@@ -22,50 +22,54 @@ def checkoutRepo(String repo, String branch = 'master', String dir, String compa
 
 return this
 
+def getRepoMap(String name, String dir = name, String owner = 'ONLYOFFICE')
+{
+    return [owner: owner, name: name, dir: dir]
+}
+
 def getReposList()
 {
     def repos = []
-    repos.add('build_tools')
-    repos.add('core')
-    repos.add('core-fonts')
-    repos.add('desktop-apps')
-    repos.add('desktop-sdk')
-    repos.add('dictionaries')
-    repos.add('document-builder-package')
-    repos.add('document-server-integration')
-    repos.add('document-server-package')
-    repos.add('plugin-ocr')
-    repos.add('plugin-macros')
-    repos.add('plugin-highlightcode')
-    repos.add('plugin-photoeditor')
-    repos.add('plugin-youtube')
-    repos.add('plugin-speech')
-    repos.add('plugin-thesaurus')
-    repos.add('plugin-translator')
-    repos.add('plugin-autocomplete')
-    repos.add('plugin-easybib')
-    repos.add('plugin-wordpress')
-    repos.add('plugin-zotero')
-    repos.add('plugin-mendeley')
-    repos.add('sdkjs')
-    repos.add('sdkjs-comparison')
-    repos.add('sdkjs-content-controls')
-    repos.add('sdkjs-disable-features')
-    repos.add('server')
-    repos.add('server-license')
-    repos.add('server-lockstorage')
-    repos.add('web-apps')
-    repos.add('web-apps-mobile')
-    repos.add('Docker-DocumentServer')
-    repos.add('DocumentBuilder')
+    repos.add(getRepoMap('build_tools'))
+    repos.add(getRepoMap('core'))
+    repos.add(getRepoMap('core-fonts'))
+    repos.add(getRepoMap('desktop-apps'))
+    repos.add(getRepoMap('desktop-sdk'))
+    repos.add(getRepoMap('dictionaries'))
+    repos.add(getRepoMap('document-builder-package'))
+    repos.add(getRepoMap('document-server-integration'))
+    repos.add(getRepoMap('document-server-package'))
+    repos.add(getRepoMap('plugin-ocr',           'sdkjs-plugins/plugin-ocr'))
+    repos.add(getRepoMap('plugin-macros',        'sdkjs-plugins/plugin-macros'))
+    repos.add(getRepoMap('plugin-highlightcode', 'sdkjs-plugins/plugin-highlightcode'))
+    repos.add(getRepoMap('plugin-photoeditor',   'sdkjs-plugins/plugin-photoeditor'))
+    repos.add(getRepoMap('plugin-youtube',       'sdkjs-plugins/plugin-youtube'))
+    repos.add(getRepoMap('plugin-speech',        'sdkjs-plugins/plugin-speech'))
+    repos.add(getRepoMap('plugin-thesaurus',     'sdkjs-plugins/plugin-thesaurus'))
+    repos.add(getRepoMap('plugin-translator',    'sdkjs-plugins/plugin-translator'))
+    repos.add(getRepoMap('plugin-autocomplete',  'sdkjs-plugins/plugin-autocomplete'))
+    repos.add(getRepoMap('plugin-easybib',       'sdkjs-plugins/plugin-easybib'))
+    repos.add(getRepoMap('plugin-wordpress',     'sdkjs-plugins/plugin-wordpress'))
+    repos.add(getRepoMap('plugin-zotero',        'sdkjs-plugins/plugin-zotero'))
+    repos.add(getRepoMap('plugin-mendeley',      'sdkjs-plugins/plugin-mendeley'))
+    repos.add(getRepoMap('sdkjs'))
+    repos.add(getRepoMap('sdkjs-comparison'))
+    repos.add(getRepoMap('sdkjs-content-controls'))
+    repos.add(getRepoMap('sdkjs-disable-features'))
+    repos.add(getRepoMap('server'))
+    repos.add(getRepoMap('server-license'))
+    repos.add(getRepoMap('server-lockstorage'))
+    repos.add(getRepoMap('web-apps'))
+    repos.add(getRepoMap('web-apps-mobile'))
+    repos.add(getRepoMap('Docker-DocumentServer'))
+    repos.add(getRepoMap('DocumentBuilder'))
     return repos
 }
 
 def checkoutRepos(String branch = 'master')
 {    
     for (repo in getReposList()) {
-        String dir = !repo.startsWith("plugin-") ? repo : "sdkjs-plugins/${repo}"
-        checkoutRepo(repo, branch, dir)
+        checkoutRepo(repo.name, branch, repo.dir, repo.owner)
     }
 
     return this
@@ -74,14 +78,204 @@ def checkoutRepos(String branch = 'master')
 def tagRepos(String tag)
 {
     for (repo in getReposList()) {
-        String dir = !repo.startsWith("plugin-") ? repo : "sdkjs-plugins/${repo}"
-        sh "cd ${dir} && \
+        sh "cd ${repo.dir} && \
             git tag -l | xargs git tag -d && \
             git fetch --tags && \
             git tag ${tag} && \
 	        git push origin --tags"
     }
 
+    return this
+}
+
+def printBranches()
+{
+    def success = 0
+    def repos = getReposList().size()
+    for (repo in getReposList()) {
+        def ret = sh (
+            label: "${repo.owner}/${repo.name}: branches",
+            script: """
+                gh api -X GET repos/${repo.owner}/${repo.name}/branches | \
+                jq -c '.[] | { name, protected }'
+            """,
+            returnStatus: true
+        )
+        if (ret == 0) {
+            success++
+        }
+    }
+
+    if (success == 0) {
+        currentBuild.result = "FAILURE"
+    } else if (success != repos) {
+        currentBuild.result = "UNSTABLE"
+    }
+    return this
+}
+
+def startRelease(String branch, String baseBranch)
+{
+    def success = 0
+    def repos = getReposList().size()
+    for (repo in getReposList()) {
+        dir (repo.dir) {
+            def ret = sh (
+                label: "${repo.owner}/${repo.name}: start ${branch}",
+                script: """
+                    if [ \$(git branch -a | grep 'develop' | wc -c) -eq 0 ]; then
+                        git checkout -f master
+                        git checkout -b develop
+                        git push origin develop
+                    fi
+                    if [ \$(git branch -a | grep '${branch}' | wc -c) -ne 0 ]; then
+                        exit 0
+                    fi
+                    git checkout -f ${baseBranch}
+                    git pull origin ${baseBranch} --ff-only
+                    git checkout -B ${branch}
+                    git push origin ${branch}
+                """,
+                returnStatus: true
+            )
+            if (ret == 0) {
+                success++
+            }
+        }
+    }
+
+    if (success == 0) {
+        currentBuild.result = "FAILURE"
+    } else if (success != repos) {
+        currentBuild.result = "UNSTABLE"
+    }
+    return this
+}
+
+def finishRelease(String branch, String extraBranch)
+{
+    def success = 0
+    def repos = getReposList().size()
+    for (repo in getReposList()) {
+        dir (repo.dir) {
+            def ret = sh (
+                label: "${repo.owner}/${repo.name}: finish ${branch}",
+                script: """#!/bin/bash -xe
+                    if [ \$(git branch -a | grep '${branch}' | wc -c) -eq 0 ]; then
+                        exit 0
+                    fi
+                    baseBranches=('master' 'develop')
+                    merge=0
+                    mergeTotal=2
+                    if [ \$(echo -n '${extraBranch}' | wc -c) -ne 0 ] \
+                    && [ \$(git branch -a | grep '${extraBranch}' | wc -c) -ne 0 ]; then
+                        ((++mergeTotal))
+                        baseBranches+=('${extraBranch}')
+                    fi
+                    for baseBranch in \${baseBranches[*]}; do
+                        git checkout -f ${branch}
+                        git pull origin ${branch} --ff-only
+                        gh pr create \
+                            --base \$baseBranch \
+                            --title \"Merge branch ${branch} into \$baseBranch\" \
+                            --body \"\" || \
+                        true
+                        git checkout \$baseBranch
+                        git pull origin \$baseBranch --ff-only
+                        git merge ${branch} \
+                            --no-edit --no-ff \
+                            -m \"Merge branch ${branch} into \$baseBranch\" || \
+                        continue
+                        git push origin \$baseBranch
+                        ((++merge))
+                    done
+                    if [ \$merge -eq \$mergeTotal ]; then
+                        gh api -X DELETE \
+                            repos/${repo.owner}/${repo.name}/branches/${branch}/protection || \
+                        true
+                        git branch -D ${branch}
+                        git push origin -d ${branch}
+                    else
+                        exit 2
+                    fi
+                """,
+                returnStatus: true
+            )
+            if (ret == 0) {
+                success++
+            }
+        }
+    }
+
+    if (success == 0) {
+        currentBuild.result = "FAILURE"
+    } else if (success != repos) {
+        currentBuild.result = "UNSTABLE"
+    }
+    return this
+}
+
+def protectRelease(String branch)
+{
+    def success = 0
+    def repos = getReposList().size()
+    for (repo in getReposList()) {
+        def ret = sh (
+            label: "${repo.owner}/${repo.name}: protect ${branch}",
+            script: """
+                echo '{
+                    "required_status_checks": null,
+                    "enforce_admins": true,
+                    "required_pull_request_reviews": null,
+                    "restrictions": {
+                        "users": [],
+                        "teams": [
+                            "dep-application-development-admins"
+                        ]
+                    }
+                }' | \
+                gh api -X PUT \
+                    repos/${repo.owner}/${repo.name}/branches/${branch}/protection \
+                    --input -
+            """,
+            returnStatus: true
+        )
+        if (ret == 0) {
+            success++
+        }
+    }
+
+    if (success == 0) {
+        currentBuild.result = "FAILURE"
+    } else if (success != repos) {
+        currentBuild.result = "UNSTABLE"
+    }
+    return this
+}
+
+def unprotectRelease(String branch)
+{
+    def success = 0
+    def repos = getReposList().size()
+    for (repo in getReposList()) {
+        def ret = sh (
+            label: "${repo.owner}/${repo.name}: unprotect ${branch}",
+            script: """
+                gh api -X DELETE \
+                    repos/${repo.owner}/${repo.name}/branches/${branch}/protection
+            """,
+            returnStatus: true
+        )
+        if (ret == 0) {
+            success++
+        }
+    }
+
+    if (success == 0) {
+        currentBuild.result = "FAILURE"
+    } else if (success != repos) {
+        currentBuild.result = "UNSTABLE"
+    }
     return this
 }
 
@@ -229,7 +423,7 @@ def linuxBuildCore()
 
 def linuxTest()
 {
-    checkoutRepo('doc-builder-testing', 'master', 'doc-builder-testing')
+    checkoutRepo('doc-builder-testing', 'master')
     sh "docker rmi doc-builder-testing || true"
     sh "cd doc-builder-testing &&\
         docker build --tag doc-builder-testing -f dockerfiles/debian-develop/Dockerfile . &&\
