dce42ace9e046388c4581015458d5a09d44660e6
Semyon Bezrukov <semen.bezrukov@onlyoffice.com>
2021-07-15 09:58:20 +0300

    Fix macos deploy

--- utils.groovy
+++ utils.groovy
@@ -210,14 +210,27 @@ void buildEditors (String platform) {
     sh "cd build_tools && ./make_packages.py"
 
     String packageName = env._X86 != '1' ? "ONLYOFFICE" : "ONLYOFFICE-x86"
+    String appName = "ONLYOFFICE"
+    String appVersion = sh (
+      script: "mdls -name kMDItemVersion -raw desktop-apps/macos/build/${appName}.app",
+      returnStdout: true).trim()
+    String zipName = "${packageName}-${appVersion}"
     String subdir = env._X86 != '1' ? "editors_x64" : "editors_x86"
     fplatform = env._X86 != '1' ? "macOS x64" : "macOS x86"
 
     dir ("desktop-apps/macos/build") {
-      uploadFiles("${packageName}.dmg",
-        "macos/${packageName}-${version}.dmg", product, fplatform, "Disk Image")
-      uploadFiles("update/*.zip,update/*.delta,update/*.xml,update/*.html",
-        "macos/${subdir}/${version}/", product, fplatform, "Sparkle")
+      uploadFiles("${packageName}.dmg", "macos/${packageName}-${version}.dmg",
+        product, fplatform, "Disk Image")
+      uploadFiles("update/${zipName}.zip", "macos/${subdir}/${version}/",
+        product, fplatform, "Sparkle")
+      uploadFiles("update/*.delta", "macos/${subdir}/${version}/",
+        product, fplatform, "Sparkle")
+      uploadFiles("update/*.xml", "macos/${subdir}/${version}/",
+        product, fplatform, "Sparkle")
+      uploadFiles("update/${zipName}.html", "macos/${subdir}/${version}/ReleaseNotes.html",
+        product, fplatform, "Sparkle")
+      uploadFiles("update/${zipName}.ru.html", "macos/${subdir}/${version}/ReleaseNotesRU.html",
+        product, fplatform, "Sparkle")
     }
 
   } else if (platform == "linux_64") {
